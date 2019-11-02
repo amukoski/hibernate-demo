0374d8a1e27417f9a3908a12e8a5c5a180b0507d
Antonio Mukoski <antonio.mukoski@seavus.com>
2019-11-02 14:43:49 +0100

    01 Unidirectional Intermediary Table

--- src/test/java/com/seavus/hibernate/demo/controller/_01_Unidirectional_IntermediaryTable.java
+++ src/test/java/com/seavus/hibernate/demo/controller/_01_Unidirectional_IntermediaryTable.java
@@ -1,7 +1,27 @@
 package com.seavus.hibernate.demo.controller;
 
+import com.seavus.hibernate.demo.domain.Cv;
+import com.seavus.hibernate.demo.domain.Language;
+import com.seavus.hibernate.demo.repository.CvRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class _01_Unidirectional_IntermediaryTable {
+
+    @Autowired
+    private CvRepository cvRepository;
+
+    @Test
+    void unidirectional_IntermediaryTable() {
+        Cv cv = new Cv();
+        cv.addLanguage(new Language("English"));
+        cv.addLanguage(new Language("Serbian"));
+        cv.addLanguage(new Language("Macedonian"));
+
+        System.err.println("****** UNIDIRECTIONAL IntermediaryTable ******");
+
+        cvRepository.save(cv);
+    }
 }
