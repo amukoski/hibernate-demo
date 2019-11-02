13b3b671c408ef0733deddf69eab95ef857d5bcd
Antonio Mukoski <antonio.mukoski@seavus.com>
2019-11-02 17:48:04 +0100

    08 FetchMode.SUBSELECT

--- src/main/java/com/seavus/hibernate/demo/domain/Cv.java
+++ src/main/java/com/seavus/hibernate/demo/domain/Cv.java
@@ -2,10 +2,12 @@ package com.seavus.hibernate.demo.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -16,14 +18,17 @@ public class Cv {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Fetch(FetchMode.SUBSELECT)
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "cv")
-    public Set<Language> languages = new HashSet<>();
+    public List<Language> languages = new ArrayList<>();
 
+    @Fetch(FetchMode.SUBSELECT)
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "cv")
-    public Set<Education> educations = new HashSet<>();
+    public List<Education> educations = new ArrayList<>();
 
+    @Fetch(FetchMode.SUBSELECT)
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "cv")
-    public Set<Project> projects = new HashSet<>();
+    public List<Project> projects = new ArrayList<>();
 
     public void addLanguage(Language language) {
         languages.add(language);
--- src/main/java/com/seavus/hibernate/demo/repository/CvRepository.java
+++ src/main/java/com/seavus/hibernate/demo/repository/CvRepository.java
@@ -1,15 +1,7 @@
 package com.seavus.hibernate.demo.repository;
 
 import com.seavus.hibernate.demo.domain.Cv;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CvRepository extends CrudRepository<Cv, Long> {
-
-    @Override
-    @Query("SELECT DISTINCT cv FROM Cv cv " +
-            "LEFT JOIN FETCH cv.languages " +
-            "LEFT JOIN FETCH cv.educations " +
-            "LEFT JOIN FETCH cv.projects")
-    Iterable<Cv> findAll();
 }
